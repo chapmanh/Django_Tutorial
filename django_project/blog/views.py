c296b9007cc2c7a1bb2e9d67515043fbd2d63475
Haydn Chapman <haydn.chapman@DECODED Haydn Chapman.local>
2020-08-04 21:00:02 +0100

    Part 5 - Database and Migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Haydn',
-        'title': 'Test post',
-        'content': '#first',
-        'date_posted': 'August 02 2020'
-    },
-    {
-        'author': 'Titus',
-        'title': 'I am awesome',
-        'content': 'The title says it all...',
-        'date_posted': 'August 03 2020'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
